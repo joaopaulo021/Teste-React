06a121cf309104ad26331d8998df14b5c96ce238
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-06-15 15:15:30 -0300

    Update App styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import './App.css'
+// import './App.css'
 import MockData from './components/MockData';
 import DataDisplay from './components/DataDisplay';
 import PopUp from './components/PopUp';
-import ConfirmPage from './pages/ConfirmPage';
+import ConfirmPage from './components/ConfirmPage';
+import { StyledDiv, StyledDivContainer } from './styles'
 
 interface DadosSimulados {
   valorParcelas: number;
@@ -17,20 +18,21 @@ const formatoReal = (valor: number): string => {
 
 const App: React.FC = () => {
   const [dadosSimulados, setDadosSimulados] = useState<DadosSimulados | null>(null);
-  const [showPopup, setShowPopup] = useState(false);
+  const [showPopup, setShowPopup] = useState(false)
   const [confirmed, setConfirmed] = useState(false)
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
   const handleMockSubmit = (valorParcelas: number, numeroParcelas: number, totalComJuros: number) => {
     setDadosSimulados({ valorParcelas, numeroParcelas, totalComJuros });
   };
+
 
   const handleConfirm = () => {
     setLoading(true);
     setShowPopup(false);
     setConfirmed(true)
     setTimeout(() => {
-      setLoading(false); // Define o estado de carregamento como falso após 2 segundos
+      setLoading(false);
       setConfirmed(true);
     }, 2000);
   };
@@ -40,47 +42,49 @@ const App: React.FC = () => {
   };
 
   return (
-    <>
-      <h1>Solicitação de empréstimo</h1>
-      {!confirmed && (
-        <>
-          <MockData onSubmit={handleMockSubmit} />
-          {dadosSimulados && (
-            <DataDisplay
-              formatoReal={formatoReal}
-              valorParcelas={dadosSimulados.valorParcelas}
-              numeroParcelas={dadosSimulados.numeroParcelas}
-              totalComJuros={dadosSimulados.totalComJuros}
-              onConfirm={() => setShowPopup(true)}
-            />
-          )}
-        </>
-      )}
-      {showPopup && (
-        <PopUp
-          formatoReal={formatoReal}
-          valorParcelas={dadosSimulados?.valorParcelas || 0}
-          numeroParcelas={dadosSimulados?.numeroParcelas || 0}
-          totalComJuros={dadosSimulados?.totalComJuros || 0}
-          onConfirm={handleConfirm}
-          onCancel={handleCancel}
-        />
-      )}
-      {confirmed && dadosSimulados && (
-        <>
-          {loading ? (
-            <p>Carregando seu empréstimo...</p>
-          ) : (
-            <ConfirmPage
-              formatoReal={formatoReal}
-              valorParcelas={dadosSimulados.valorParcelas}
-              numeroParcelas={dadosSimulados.numeroParcelas}
-              totalComJuros={dadosSimulados.totalComJuros}
-            />
-          )}
-        </>
-      )}
-    </>
+    <StyledDivContainer>
+      <StyledDiv>
+        {!confirmed && (
+          <>
+            <MockData onSubmit={handleMockSubmit} />
+            {dadosSimulados && (
+              <DataDisplay
+                formatoReal={formatoReal}
+                valorParcelas={dadosSimulados.valorParcelas}
+                numeroParcelas={dadosSimulados.numeroParcelas}
+                totalComJuros={dadosSimulados.totalComJuros}
+                onConfirm={() => setShowPopup(true)}
+              />
+            )}
+          </>
+        )}
+        {showPopup && (
+          <PopUp
+            formatoReal={formatoReal}
+            valorParcelas={dadosSimulados?.valorParcelas || 0}
+            numeroParcelas={dadosSimulados?.numeroParcelas || 0}
+            totalComJuros={dadosSimulados?.totalComJuros || 0}
+            onConfirm={handleConfirm}
+            onCancel={handleCancel}
+          />
+        )}
+        {confirmed && dadosSimulados && (
+          <>
+            {loading ? (
+              <p>Carregando seu empréstimo...</p>
+            ) : (
+              <ConfirmPage
+                formatoReal={formatoReal}
+                valorParcelas={dadosSimulados.valorParcelas}
+                numeroParcelas={dadosSimulados.numeroParcelas}
+                totalComJuros={dadosSimulados.totalComJuros}
+              />
+            )}
+          </>
+        )}
+      </StyledDiv>
+    </StyledDivContainer>
+
   )
 }
 
---
+++ src/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  width:100%;
+  flex-direction:column;
+`;
+
+export const StyledDivContainer = styled.div`
+  display: flex;
+  padding:20px;
+  align-items:center;
+  justify-content:center;
+  flex-direction:row;
+`;
+
+
+
